115128e59a01a41b652bde9948f29942b907216b
Francesco Venturoli <f.venturoli@gmail.com>
2016-10-12 20:53:39 +0200

    Everything works now, some explicative comments added

--- Looper/Looper.ino
+++ Looper/Looper.ino
@@ -1,5 +1,5 @@
-#include <EEPROM.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 #include <MIDI.h>
 #include <midi_Defs.h>
 #include <midi_Message.h>
@@ -7,9 +7,19 @@
 #include <midi_Settings.h>
 
 // Create a software serial port to allow debugging over USB
-SoftwareSerial midiSerial(2, 3);
+//SoftwareSerial midiSerial(2, 3); // RX, TX
 // Create instance of MIDI library, using software serial
-MIDI_CREATE_INSTANCE(SoftwareSerial, midiSerial, midiLooper);
+//MIDI_CREATE_INSTANCE(SoftwareSerial, midiSerial, midiLooper);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, midiLooper);
+
+// Onboard LED
+int ledPin = 13;
+//Pin connected to ST_CP of 74HC595
+int latchPin = 8;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 12;
+////Pin connected to DS of 74HC595
+int dataPin = 11;
 
 // Configuration
 struct configuration {
@@ -25,6 +35,7 @@ byte currentOutput;
 // Handlers
 // Control Change: switches a single loop on or off
 void handleControlChange(byte channel, byte number, byte value) {
+  //Serial.println("CC");
   for (int i = 0 ; i < 8 ; i++) {
     // Individual loops will be switched on and off using bit masks for higher efficiency
     // Check if CC number received matches with one of the loops
@@ -39,12 +50,28 @@ void handleControlChange(byte channel, byte number, byte value) {
         // Sets a single bit to 0
         currentOutput = currentOutput & (~mask);
       }
+      updateOutput();
     }
   }
+  // Store
+  if (number == 22) {
+    // Edit config
+    config.output[value] = currentOutput;
+    // Store into EEPROM
+    int eeAddress = 0;
+    digitalWrite(ledPin, HIGH);
+    EEPROM.put(eeAddress, config);
+    digitalWrite(ledPin, LOW);
+  }
+  //Serial.print("output: ");
+  //Serial.println(currentOutput, BIN);
 }
 // Program Change: loads a specific output sequence
 void handleProgramChange(byte channel, byte number) {
   currentOutput = config.output[number];
+  updateOutput();
+  //Serial.print("output: ");
+  //Serial.println(currentOutput, BIN);
 }
 
 // Main
@@ -58,7 +85,9 @@ void initEEPROM() {
     config.output[i] = B00000000;
   }
   int eeAddress = 0;
+  digitalWrite(ledPin, HIGH);
   EEPROM.put(eeAddress, config);
+  digitalWrite(ledPin, LOW);
 }
 
 void printConfig() {
@@ -77,21 +106,50 @@ void printConfig() {
   };
 }
 
+void updateOutput() {
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+    // shift out the bits:
+    shiftOut(dataPin, clockPin, MSBFIRST, currentOutput);  
+    //take the latch pin high so the LEDs will light up:
+    digitalWrite(latchPin, HIGH);
+}
+
 void setup() {
-  Serial.begin(9600);
-  //initEEPROM();
+  // Sets the pins used for the shift register as output
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  
+  // LED pin, used to monitor EEPROM activity
+  pinMode(ledPin, OUTPUT);
+  
+  // Uncomment this line if the EEPROM needs to be initialized again
+  // initEEPROM();
+  
+  // Read configuration from EEPROM
   int eeAddress = 0;
   EEPROM.get(eeAddress, config);
-  printConfig();
+
+  // Sets first output
   currentOutput = config.output[0];
-  // put your setup code here, to run once:
+  updateOutput();
+  
+  // Disable MIDI Thru
+  midiLooper.turnThruOff();
+  
+  // Sets event handlers
   midiLooper.setHandleControlChange(handleControlChange);
   midiLooper.setHandleProgramChange(handleProgramChange);
-  midiLooper.turnThruOff();
+
+  // Prepares MIDI interface for listening
   midiLooper.begin(MIDI_CHANNEL_OMNI);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Keep reading (polling incoming MIDI messages)
+  // Return value isn't checked because everything happens in the handlers
   midiLooper.read();
 }
+
